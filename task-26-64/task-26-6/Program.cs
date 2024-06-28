7bfe52f4d22bdaeca93379d55a723f39cc44e338
a7madddddd <142698638+a7madddddd@users.noreply.github.com>
2024-06-28 12:35:43 +0300

    edit

--- task-26-64/task-26-6/Program.cs
+++ task-26-64/task-26-6/Program.cs
@@ -8,129 +8,101 @@ namespace task_26_64
         {
 
             //q1
-            int x = 7;
-            int y = 6;
+            //Console.WriteLine("task-1");
+            //int[] numbes = { 4, 7};
+            //Array.Sort(numbes);
+            //Console.WriteLine(numbes[0]+" is smaller");
+            //Console.WriteLine("...............");
 
-            if (x > y)
-            {
-                Console.WriteLine("7 greater than 6");
-            }
-            else
-            {
-                Console.WriteLine("6 greater than 7");
-            }
 
-            Console.WriteLine("enter another number");
+
             //q2
-            int a = int.Parse(Console.ReadLine());
-            Console.WriteLine(a);
-            if (a > 0)
-            {
-                Console.WriteLine("The sign is +");
-            }
-            else if (a < 0)
-            {
-                Console.WriteLine("The sign is -");
-            }
-            else
-            {
-                Console.WriteLine("The sign is 0");
-            };
+            //Console.WriteLine("task-2");
+            //int num3 = 2;
+            //int num4 = -4;
+            //int sign1 = Math.Sign(num3);
+            //int sign2 = Math.Sign(num4);
+
+            //string sign3 = num3.ToString("+#;-#;0");
+            //string sign4 = num4.ToString("+#;-#;0");
+            //Console.WriteLine("the sign of 2 is " + sign3[0]);
+            //Console.WriteLine("the sign of -4 is " + sign4[0]);
+            //Console.WriteLine("");
+            //Console.WriteLine(".................");
+
+
+
 
             //q3
-            Console.WriteLine("sorting array of number: ");
-
-            int[] arr1 = new int[] { 0, -1, 4 };
-            Array.Sort(arr1);
-            foreach (int i in arr1) { Console.WriteLine(i); }
+            //Console.WriteLine("task-3");
+            //Console.WriteLine(".sorting array of number: { 0, -1, 4 }");
+            //int[] arr1 = { 0, -1, 4 };
+            //Array.Sort(arr1);
+            //Console.WriteLine(arr1[0]);
+            //Console.WriteLine(arr1[1]);
+            //Console.WriteLine(arr1[2]);
+            //Console.WriteLine("....................");
 
 
 
 
             //q4
             //-5, -2, -6, 0, -1 
-            if (-5 > -2 && -5 > -6 && -5 > 0 && -5 > -1)
-            {
-                Console.WriteLine("-5 is greater than all number");
-            }
-            else if (-2 > -5 && -2 > -6 && -2 > 0 && -2 > -1)
-            {
-                Console.WriteLine("-2 is greater than all number");
-            }
-            else if (-6 > -5 && -6 > -2 && -6 > 0 && -6 > -1)
-            {
-                Console.WriteLine("-6 is greater than all number");
-
-            }
-            else if (0 > -5 && 0 > -2 && 0 > -6 && 0 > -1)
-            {
-                Console.WriteLine("0 is greater than all number");
-
-            }
-            else
-            {
-                Console.WriteLine("0 is greater than all number");
-            }
+            //Console.WriteLine("task-4");
+            //int[] ar = { -5, -2, -6, 0, -1};
+            //Array.Sort(ar);
+            //Console.WriteLine(ar);
+            //int max = ar[ar.Length - 1];
+            //Console.WriteLine($"the max number in array is: {max}");
+            //Console.WriteLine("....................");
 
 
             //q5
+            //Console.WriteLine("task-5");
+            //Console.WriteLine("add kilometers");
+            //int entered =Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("add kilometers");
-            string entered = Console.ReadLine();
-
-            double kilometers;
-            bool Space = double.TryParse(entered, out kilometers);
-            if (Space)
-            {
-                double milesInHour = kilometers * 0.621371;
-                Console.WriteLine("{0} kilometers per hour is equal to {1} miles per hour.", kilometers, milesInHour);
-            }
-
+            //double MileToKm = entered * 0.621371;
+            //Console.WriteLine($"your kilometers per hours to mile ={MileToKm} per hours");
+            //Console.WriteLine("......................");
 
             //q6
 
-            Console.WriteLine("enter an hours ");
-            string enterd = Console.ReadLine();
-            Console.WriteLine("enter an minuts");
-            string enterd2 = Console.ReadLine();
+            //Console.WriteLine("task-6");
+            //Console.WriteLine("enter an hours ");
+            //int enter1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter an minuts");
+            //int enter2 = Convert.ToInt32(Console.ReadLine());
 
-            int hours;
-            int minutes;
-            bool hourstominuts = int.TryParse(enterd, out hours);
-            bool minutstohours = int.TryParse(enterd2, out minutes);
-            if (hourstominuts && minutstohours)
-            {
-                int finalTime = (hours * 60) + minutes;
-                Console.WriteLine("{0} minuts", finalTime);
-            }
+            //int hoursToMinutes = enter1 * 60;
+            //int total = hoursToMinutes + enter2;
 
+            //Console.WriteLine($" minutes in hours = {total}");
+            //Console.WriteLine("..................");
 
 
             //q7
+            //Console.WriteLine("task-7");
+            //Console.WriteLine("enter minutes");
+            //double m1 =Convert.ToDouble(Console.ReadLine());
+            //double minToHour = m1 / 60;
+            //Console.WriteLine($"minutes in hours = {minToHour}hour");
+            //Console.WriteLine("...............");
 
-            Console.WriteLine("enter minutes");
-            string m1 = Console.ReadLine();
 
 
-            int m2;
-            bool minutsToHours = int.TryParse(m1, out m2);
-            if (minutsToHours)
-            {
-                int hour = m2 / 60;
-                int min = m2 % 60;
-                Console.WriteLine("{0} hour, {1}imnutes ", hour, min);
-            }
+
 
 
 
             //q8
 
-            string[] different = { "ahmad", "onizat", "love sleep", "love coffee so much", "and this is good" };
-            Console.WriteLine(different[0].Substring(0, 5));
-            Console.WriteLine(different[1].Substring(0, 5));
-            Console.WriteLine(different[2].Substring(0, 5));
-            Console.WriteLine(different[3].Substring(0, 5));
-            Console.WriteLine(different[4].Substring(0, 5));
+            //string[] different = { "ahmad", "onizat", "love sleep", "love coffee so much", "and this is good" };
+            //Console.WriteLine(different[0].Substring(0, 5));
+            //Console.WriteLine(different[1].Substring(0, 5));
+            //Console.WriteLine(different[2].Substring(0, 5));
+            //Console.WriteLine(different[3].Substring(0, 5));
+            //Console.WriteLine(different[4].Substring(0, 5));
 
 
 
