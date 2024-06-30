a1a532fb348e81bcad3325c72dd21950da414cf4
a7madddddd <142698638+a7madddddd@users.noreply.github.com>
2024-06-30 22:59:38 +0300

    task-30-6

--- task-27.06.2024/task-27.06.2024/Program.cs
+++ task-27.06.2024/task-27.06.2024/Program.cs
@@ -15,141 +15,141 @@ namespace task_27._06._2024
             //q1-a
 
 
-            //Console.WriteLine("task 1-a");
-            //int[] ARR = { 1, 7, 9, 45 };
-            //foreach (int i in ARR)
-            //{
+            Console.WriteLine("task 1-a");
+            int[] ARR = { 1, 7, 9, 45 };
+            foreach (int i in ARR)
+            {
 
-            //    Console.WriteLine(i);
-            //};
-            //Console.WriteLine(".........");
+                Console.WriteLine(i);
+            };
+            Console.WriteLine(".........");
 
 
             //q1-b
 
 
-            //Console.WriteLine("task 1-b");
-            //string[] arr2 = { "Str", "alex", "moh" };
-            //foreach (string i in arr2)
-            //{
-            //    Console.WriteLine(i);
-            //};
-            //Console.WriteLine(".........");
+            Console.WriteLine("task 1-b");
+            string[] arr2 = { "Str", "alex", "moh" };
+            foreach (string i in arr2)
+            {
+                Console.WriteLine(i);
+            };
+            Console.WriteLine(".........");
 
 
             // q1-c
 
 
-            //Console.WriteLine("task 1-c");
-            //string[] arr3 = { "the", "fox,", "over", "lazy", "dog" };
-            //foreach (string i in arr3)
-            //{
-            //    Console.WriteLine(i);
-            //};
-            //Console.WriteLine(".........");
+            Console.WriteLine("task 1-c");
+            string[] arr3 = { "the", "fox,", "over", "lazy", "dog" };
+            foreach (string i in arr3)
+            {
+                Console.WriteLine(i);
+            };
+            Console.WriteLine(".........");
 
 
             //q2 
 
-            //Console.WriteLine("task-2");
-            //String[] fruits = { "Tomato", "Banana", "Watermelon" };
+            Console.WriteLine("task-2");
+            String[] fruits = { "Tomato", "Banana", "Watermelon" };
 
-            //var tomato = Array.IndexOf(fruits, "Tomato");
-            //var banana = Array.IndexOf(fruits, "Banana");
-            //Console.WriteLine($"index of tomato {tomato}");
-            //Console.WriteLine($"index of banana {banana}");
+            var tomato = Array.IndexOf(fruits, "Tomato");
+            var banana = Array.IndexOf(fruits, "Banana");
+            Console.WriteLine($"index of tomato {tomato}");
+            Console.WriteLine($"index of banana {banana}");
 
-            //Console.WriteLine(".................");
+            Console.WriteLine(".................");
 
 
             //q3
 
-            //Console.WriteLine("task-3");
-            //string[] favoriteFood = { "sea food", "fruite salad", "steak", "ksbsa", "mansaf" };
-            //string[] favoriteSports = { "footbal", "basketball", "tennis" };
-            //string[] favoriteMovies = { "game of theones", "peaky blainders", "sparta" };
+            Console.WriteLine("task-3");
+            string[] favoriteFood = { "sea food", "fruite salad", "steak", "ksbsa", "mansaf" };
+            string[] favoriteSports = { "footbal", "basketball", "tennis" };
+            string[] favoriteMovies = { "game of theones", "peaky blainders", "sparta" };
 
-            //Console.WriteLine("//////foreach loop///////");
-            //foreach (string i in favoriteFood)
-            //{
-            //    Console.WriteLine(i);
-            //}
-            //Console.WriteLine("..........");
-            //Console.WriteLine("/////for loop//////");
-            //for (int i = 0; i < favoriteFood.Length; i++)
-            //{
-            //    Console.WriteLine(favoriteFood[i]);
-            //}
-            //Console.WriteLine("..........");
+            Console.WriteLine("//////foreach loop///////");
+            foreach (string i in favoriteFood)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("..........");
+            Console.WriteLine("/////for loop//////");
+            for (int i = 0; i < favoriteFood.Length; i++)
+            {
+                Console.WriteLine(favoriteFood[i]);
+            }
+            Console.WriteLine("..........");
 
 
 
             //q4
 
-            //Console.WriteLine("task-4");
-            //Console.WriteLine("enter three numbers with comma/,/ betwwen them :");
-            //string numbersInput = Console.ReadLine();
-            //string[] numbers = numbersInput.Split(',');
-            //int firstNumber = int.Parse(numbers[0]);
-            //int secondNumber = int.Parse(numbers[1]);
-            //int thierdNumber = int.Parse(numbers[2]);
-            //int total = firstNumber + secondNumber + thierdNumber;
-            //Console.WriteLine($"the total number of your input is : {total}");
-            //Console.WriteLine("............");
+            Console.WriteLine("task-4");
+            Console.WriteLine("enter three numbers with comma/,/ betwwen them :");
+            string numbersInput = Console.ReadLine();
+            string[] numbers = numbersInput.Split(',');
+            int firstNumber = int.Parse(numbers[0]);
+            int secondNumber = int.Parse(numbers[1]);
+            int thierdNumber = int.Parse(numbers[2]);
+            int total = firstNumber + secondNumber + thierdNumber;
+            Console.WriteLine($"the total number of your input is : {total}");
+            Console.WriteLine("............");
 
 
             //q5
 
-            //Console.WriteLine("task-5");
-            //Console.WriteLine("enter a number :");
-            //int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("task-5");
+            Console.WriteLine("enter a number :");
+            int n = int.Parse(Console.ReadLine());
 
-            //int positive = Math.Abs(n);
-            //Console.WriteLine(positive);
-            //int s = 0;
-            
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    if (i % 2 != 0)
-            //    {
-            //        Console.WriteLine($"the odd numbers is :{i}");
-            //        s+=i;
-                    
-            //    }
-            //};
-            //Console.WriteLine($"total is {s}");
-            //Console.WriteLine(".........");
+            int positive = Math.Abs(n);
+            Console.WriteLine(positive);
+            int s = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine($"the odd numbers is :{i}");
+                    s += i;
+
+                }
+            };
+            Console.WriteLine($"total is {s}");
+            Console.WriteLine(".........");
 
             //q6
-            //Console.WriteLine("task-6");
-            //for (var item = 1; item < 6; item++)
-            //{
-            //    for (var b = 1; b < item; j++)
-            //    {
-            //        //Console.Write("  ");
-            //        Console.Write("*");
-            //    }
-            //    Console.WriteLine();
-            //};
+            Console.WriteLine("task-6");
+            for (var item = 1; item < 6; item++)
+            {
+                for (var b = 1; b < item; b++)
+                {
+                    //Console.Write("  ");
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            };
 
-            //Console.WriteLine("...........");
+            Console.WriteLine("...........");
 
 
 
             //q7
 
-            //int اورنج = 1;
-            //Console.WriteLine("task-7");
-            //for (var i = 0; i < 5; i++)
-            //{
-            //    for (var q = 1; q < i; q++)
-            //    {
-            //        Console.Write(اورنج);
-            //        اورنج++;
-            //    };
-            //    Console.WriteLine();
-            //};
-            //Console.WriteLine("............");
+            int اورنج = 1;
+            Console.WriteLine("task-7");
+            for (var i = 0; i < 5; i++)
+            {
+                for (var q = 1; q < i; q++)
+                {
+                    Console.Write(اورنج);
+                    اورنج++;
+                };
+                Console.WriteLine();
+            };
+            Console.WriteLine("............");
 
 
 
